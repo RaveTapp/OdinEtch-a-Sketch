48af869ff6009eef8051419328ef960c0b426f79
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-20 05:34:56 +0200

    Link files and create main container

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("div.container");
+
+const square = document.createElement("div");
+
+square.style.width = "20px";
+square.style.length = "20px";
+square.textContent = "1";
+
+container.appendChild(square);
