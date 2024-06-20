e80c1309f3c1bed4cff39d0dfa0f81f82160f03e
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-20 06:10:32 +0200

    Add mouse event to leave a color trail

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const square = document.createElement("div");
 
 square.style.width = "20px";
 square.style.length = "20px";
-square.textContent = "1";
 square.setAttribute("class", "grid");
 
 let gridContainer = document.createElement("div");
@@ -13,9 +12,16 @@ gridContainer.setAttribute("class", "gridContainer");
 for(let i = 0; i < 16; i++){
     let gridContainerClone = gridContainer.cloneNode(true);
     container.appendChild(gridContainerClone);
-    for(let i = 0; i < 16; i++){
+    
+    for(let j = 0; j < 16; j++){
         let squareClone = square.cloneNode(true);
+        squareClone.textContent = i*16 + j;
         gridContainerClone.appendChild(squareClone);
+        squareClone.addEventListener("mouseenter", (e) =>{
+            //console.log(e.target);
+            //gridContainer.classList.add("hover");
+            e.target.style.backgroundColor = "red";
+        });
     }
 }
 
