35a24f2ee97b5312dd4d1c848e0418c211dddf41
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-20 05:49:36 +0200

    Add 16x16 grid

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ const square = document.createElement("div");
 square.style.width = "20px";
 square.style.length = "20px";
 square.textContent = "1";
+square.setAttribute("class", "grid");
 
-container.appendChild(square);
+let gridContainer = document.createElement("div");
+gridContainer.setAttribute("class", "gridContainer");
+
+for(let i = 0; i < 16; i++){
+    let gridContainerClone = gridContainer.cloneNode(true);
+    container.appendChild(gridContainerClone);
+    for(let i = 0; i < 16; i++){
+        let squareClone = square.cloneNode(true);
+        gridContainerClone.appendChild(squareClone);
+    }
+}
+
+
