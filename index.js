c08e5577a6f5c764e1d1c8769737767066bd3248
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-06-20 06:21:23 +0200

    Add grid generation into a function

--- index.js
+++ index.js
@@ -10,28 +10,31 @@ square.setAttribute("class", "grid");
 let gridContainer = document.createElement("div");
 gridContainer.setAttribute("class", "gridContainer");
 
-for(let i = 0; i < 16; i++){
-    let gridContainerClone = gridContainer.cloneNode(true);
-    container.appendChild(gridContainerClone);
+function createGrid(){
+    for(let i = 0; i < gridSize; i++){
+        let gridContainerClone = gridContainer.cloneNode(true);
+        container.appendChild(gridContainerClone);
+        
+        for(let j = 0; j < gridSize; j++){
+            let squareClone = square.cloneNode(true);
+            squareClone.textContent = i*gridSize + j;
+            gridContainerClone.appendChild(squareClone);
     
-    for(let j = 0; j < 16; j++){
-        let squareClone = square.cloneNode(true);
-        squareClone.textContent = i*16 + j;
-        gridContainerClone.appendChild(squareClone);
-        squareClone.addEventListener("mouseenter", (e) =>{
-            //console.log(e.target);
-            //gridContainer.classList.add("hover");
-            e.target.style.backgroundColor = "red";
-        });
+            squareClone.addEventListener("mouseenter", (e) =>{
+                e.target.style.backgroundColor = "red";
+            });
+        }
     }
 }
+
+
 
 const button = document.querySelector("#askSize");
 
 button.addEventListener("click", () =>{
-    while (gridSize < 1 || gridSize > 100){
+    do {
         gridSize = prompt("Enter new grid size");
-    }
-    
+    } while (gridSize < 1 || gridSize > 100)
+    createGrid();
 })
 
